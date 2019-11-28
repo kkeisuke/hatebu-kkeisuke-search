4c4e00d67738caaffc28ac4d29f6936a92d848d3
kimura keisuke <mail@kkeisuke.com>
2019-11-28 16:47:33 +0900

    .eslintrc.js 追加
    no-console 追加

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ],
+  rules: {
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'error',
+    'vue/script-indent': 'error',
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': 'error',
+    'vue/v-on-function-call': 'error',
+    'no-debugger': process.env.NODE_ENV === 'development' ? 'off' : 'error', // ローカル環境のみ off
+    'no-console': process.env.NODE_ENV === 'development' ? 'off' : 'error' // ローカル環境のみ off
+  }
+}
