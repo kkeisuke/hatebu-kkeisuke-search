06969ed9d516113f2420bbb0c3aa2a43a968d8eb
kimura keisuke <mail@kkeisuke.com>
2019-12-08 00:21:10 +0900

    markdown 変換処理を追加
    DOMPurify でサニタイズ、 `target="_blank"` 追加

---
+++ src/plugins/markdown.ts
@@ -0,0 +1,13 @@
+import marked from 'marked'
+import DOMPurify from 'dompurify'
+
+// https://github.com/cure53/DOMPurify/blob/master/demos/hooks-target-blank-demo.html
+DOMPurify.addHook('afterSanitizeAttributes', function(node) {
+  if ('target' in node) {
+    ;(node as HTMLElement).setAttribute('target', '_blank')
+  }
+})
+
+export const markdown = (text: string) => {
+  return DOMPurify.sanitize(marked(text))
+}
