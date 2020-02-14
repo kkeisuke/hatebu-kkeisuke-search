2d73957383218893f98ab2e42c535646abeb9084
kimura keisuke <mail@kkeisuke.com>
2020-02-14 11:03:40 +0900

    依存関係、プラグインの更新

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -1,11 +1,12 @@
 import algoliasearch from 'algoliasearch'
+import { SearchResult } from '@/store/modules/algolia/algolia'
 
 const client = algoliasearch(process.env.VUE_APP_ALGOLIA_APPLICATION, process.env.VUE_APP_ALGOLIA_API_KEY)
 const index = client.initIndex(process.env.VUE_APP_ALGOLIA_INDEX)
 export const hitsPerPage = 365
 
 export const searchAlgolia = async (query: string) => {
-  const { hits } = await index.search({ query, hitsPerPage })
+  const { hits } = await index.search(query, { hitsPerPage })
 
-  return hits
+  return hits as SearchResult[]
 }
